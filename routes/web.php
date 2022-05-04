31923b5be1fbac0a7f0c09061863ae99412738b3
Marco20TE05412 <rodriguezmendezmarcoantonioh@gmail.com>
2022-05-04 10:30:31 -0500

    ahahahah

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 //04/05/2022
-
+// ahora porque no sale 
 
 //xdxdxd
 //wenas nocheeeeeesssssssssssss
 
 //hola mundoooooooooooooooooooooo
+
 
 Route::get('/', function () {
     return view('welcome');
